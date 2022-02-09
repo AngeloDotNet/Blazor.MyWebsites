8d432127f4672abb667c15cd90487fc22d513242
Angelo Pirola <angelo9871@gmail.com>
2022-02-09 19:05:19 +0100

    Aggiunti ViewModels

---
+++ src/Blazor.MyWebsites/Shared/Models/ViewModels/ProfessionalExperience.cs
@@ -0,0 +1,25 @@
+﻿using Blazor.MyWebsites.Shared.Models.Entities;
+
+namespace Blazor.MyWebsites.Shared.Models.ViewModels
+{
+    public partial class ProfessionalExperience
+    {
+        public int Id { get; set; }
+        public string Esperienza { get; set; }
+        public string Periodo { get; set; }
+        public string Azienda { get; set; }
+        public string Luogo { get; set; }
+
+        public static ProfessionalExperience FromEntity(EsperienzaProfessionale skill)
+        {
+            return new ProfessionalExperience
+            {
+                Id = skill.Id,
+                Esperienza = skill.Esperienza,
+                Periodo = skill.Periodo,
+                Azienda = skill.Azienda,
+                Luogo = skill.Luogo
+            };
+        }
+    }
+}
---
+++ src/Blazor.MyWebsites/Shared/Models/ViewModels/SkillTecnica.cs
@@ -0,0 +1,21 @@
+﻿using Blazor.MyWebsites.Shared.Models.Entities;
+
+namespace Blazor.MyWebsites.Shared.Models.ViewModels
+{
+    public class SkillTecnica
+    {
+        public int Id { get; set; }
+        public string AreaTecnica { get; set; }
+        public string Competenza { get; set; }
+
+        public static SkillTecnica FromEntity(CompetenzaTecnica skill)
+        {
+            return new SkillTecnica
+            {
+                Id = skill.Id,
+                AreaTecnica = skill.AreaTecnica,
+                Competenza = skill.Competenza
+            };
+        }
+    }
+}
