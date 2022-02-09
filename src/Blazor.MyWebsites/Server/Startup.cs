f4338590101d9d6b32ae5754cc967f40506e74ec
Angelo Pirola <angelo9871@gmail.com>
2022-02-09 19:15:34 +0100

    Aggiornati metodi delle classi Program e Startup

--- src/Blazor.MyWebsites/Server/Startup.cs
+++ src/Blazor.MyWebsites/Server/Startup.cs
@@ -1,11 +1,12 @@
+using Blazor.MyWebsites.Server.Models.Enums;
+using Blazor.PersonalWebSites.Server.Models.Services.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Linq;
+using Microsoft.OpenApi.Models;
 
 namespace Blazor.MyWebsites.Server
 {
@@ -17,23 +18,72 @@ namespace Blazor.MyWebsites.Server
         }
 
         public IConfiguration Configuration { get; }
+        private readonly string _policyName = "CorsPolicy";
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy(name: _policyName, builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
+            var persistence = Persistence.EfCore;
+            switch (persistence)
+            {
+                case Persistence.EfCore:
+                    services.AddDbContextPool<MyAppDbContext>(optionBuilder =>
+                    {
+                        string connectionString = Configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
+                        optionBuilder.UseSqlite(connectionString);
+                    });
+
+                    break;
+            }
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Blazor.PersonalWebSites",
+                    Version = "v1",
+                    // Description = "API example that returns the current time",
+                    // TermsOfService = new Uri("https://example.com/terms"), 
+
+                    // Contact = new OpenApiContact
+                    // {
+                    //     Name = "Nominativo contatto",
+                    //     Email = "Email contatto",
+                    //     Url = new Uri("https://twitter.com/username-contatto"),
+                    // },
+
+                    // License = new OpenApiLicense
+                    // {
+                    //     Name = "Nome licenza API",
+                    //     Url = new Uri("https://example.com/license"),
+                    // }
+                });
+            });
+
+            //services.AddTransient<IWebSitesService, EfCoreWebSitesService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseWebAssemblyDebugging();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blazor.PersonalWebSites v1"));
             }
             else
             {
@@ -47,6 +97,7 @@ namespace Blazor.MyWebsites.Server
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseCors(_policyName);
 
             app.UseEndpoints(endpoints =>
             {
